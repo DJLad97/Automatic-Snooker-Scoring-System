b929dd5152dfeb2d7ff541c8eb6eb8c650f0c829
Dan <b5014133@my.shu.ac.uk>
2019-01-15 21:44:33 +0000

    Added colours for real life video

--- index.py
+++ index.py
@@ -36,7 +36,9 @@ ssh_port = 22
 # If the input is the camera, pass 0 instead of the video file name
 # cap = cv2.VideoCapture('/Users/dan/Lady-Cannings.mp4')
 
-cap = cv2.VideoCapture('snooker-game.mp4')
+# cap = cv2.VideoCapture("udp://127.0.0.1:10000")
+cap = cv2.VideoCapture('game2.mov')
+# cap = cv2.VideoCapture(0)
 # cap = cv2.VideoCapture('/Users/dan/Downloads/snooker-game2.mp4')
 
 # Check if camera opened successfully
@@ -45,25 +47,38 @@ if (cap.isOpened() == False):
 
 tableLower = (47, 100, 100)
 tableUpper = (67, 255, 255)
-
 lower = {
-  'red':    (166,84,141),
-  'yellow': (23,59,119),
-  'green':  (66,122,129),
-  'brown':  (0,29,0),
-  'blue':   (97,100,117),
-  'pink':   (141,40,200),
-  'black':  (53,26,0),
+#   #'red':    (166,84,141), // Video game colours
+  'red':    (0,176,154),
+# #   'yellow': (23,59,119), // Video game colours
+  'yellow': (11,139,215),
+# #   'green':  (66,122,129), // Video game colours
+  'green':  (57,70,89),
+# #   'brown':  (0,29,0), // Video game colours
+  'brown':  (0,41,49),
+# #   'blue':   (97,100,117), // Video game colours
+  'blue':   (87,122,111),
+# #   'pink':   (141,40,200), // Video game colours
+  'pink':   (169,76,149),
+# #   'black':  (53,26,0), // Video game colours
+  'black':  (103,56,16),
 }
 
 upper = {
-  'red':    (186,255,255),
-  'yellow': (50,255,255),
-  'green':  (86,255,255),
-  'brown':  (48,174,143),
-  'blue':   (117,255,255),
-  'pink':   (169,192,255),
-  'black':  (120,120,75),
+# #   'red':    (186,255,255), // Video game colours
+  'red':    (35,237,208),
+# #   'yellow': (50,255,255), // Video game colours
+  'yellow': (169,227,255),
+# #   'green':  (86,255,255), // Video game colours
+  'green':  (82,96,138),
+# #   'brown':  (48,174,143), // Video game colours
+  'brown':  (13,204,145),
+# #   'blue':   (117,255,255), // Video game colours
+  'blue':   (162,255,255),
+# #   'pink':   (169,192,255), // Video game colours
+  'pink':   (176,199,255),
+# #   'black':  (120,120,75), // Video game colours
+  'black':  (133,156,88),
 }
 
 colours = {
@@ -97,21 +112,54 @@ finalMask = 0
 totalReds = 15
 redCount = 0
 yellowCount = 0
-initialFrame = 700
+pinkCount = 0
+initialFrame = 0
 frameCheck = 5
 currentBallCount = 0
 
 cap.set(cv2.CAP_PROP_POS_FRAMES, initialFrame)
+# img = cv2.imread('red.jpg')
+# gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+# blur = cv2.GaussianBlur(img, (9,9), 0)
 
+# hsv = cv2.cvtColor(blur, cv2.COLOR_BGR2HSV)
+# kernal = np.ones((8,8), np.uint8)
+# mask = cv2.inRange(hsv, lower['red'], upper['red'])
+# mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
+# mask = cv2.erode(mask, kernal, iterations=4)
+
+# # Add this and the threshold function to get make the results more circular 
+# # They are currently not in as they're not needed at this stage and just slow things down
+# mask = cv2.GaussianBlur(mask, (7,7), 0)
+
+# finalMask = mask
+# thresh = cv2.threshold(finalMask, 20, 255, 0)[-1]
+
+# contours = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
+# currentColor = colours['red']
+# # print(key + ': ' + str(len(contours)))
+# for c in contours:
+
+#     # The following code is only needed to show what has been detected
+#     # Can be removed in final version to improve fps
+#     M = cv2.moments(c)
+#     if M["m00"] != 0:
+#         cX = int(M["m10"] / M["m00"])
+#         cY = int(M["m01"] / M["m00"])
+#     else:
+#         cX, cY = 0, 0
+
+#     cv2.circle(img, (cX, cY), 10, (255,255,255), -1)
+# cv2.imshow('result', img)
 # Read until video is completed
 while True:
-  # Captures the live stream frame-by-frame
+    # Captures the live stream frame-by-frame
     _, frame = cap.read()
 
     # Width: 600 - Height: 337
     frame = imutils.resize(frame, width=600)
     # Crop video to only check the table and not the game UI
-    frame = frame[25:315, 25:585]
+    # frame = frame[25:315, 25:585]
     finalMask = 0
     finalMaskTable = 0
     finalEdges = 0
@@ -128,49 +176,55 @@ while True:
     # So check if current frame is 1 + initalFrame will seed the inital redCount and not update the database prematurely
     if((currentFrame == initialFrame + 1) or currentFrame % frameCheck == 0):
         redCount = 0
+        yellowCount = 0
+        pinkCount = 0
         checkBallCount = True
     else:
         checkBallCount = False
     
-    i = 0
     for key, value in upper.items():
-        contours = 0
-        kernal = np.ones((6,6), np.uint8)
+        kernal = np.ones((8,8), np.uint8)
         mask = cv2.inRange(hsv, lower[key], upper[key])
         mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN, kernal)
 
         # Add this and the threshold function to get make the results more circular 
         # They are currently not in as they're not needed at this stage and just slow things down
-        # mask = cv2.GaussianBlur(mask, (7,7), 0)
+        mask = cv2.GaussianBlur(mask, (7,7), 0)
 
         finalMask = mask
         # thresh = cv2.threshold(finalMask, 127, 255, 0)[-1]
         
-        # BUG: The length of the contours is not been reset after the ball colours been counted
-        # So 15 reds are counted, the yellow count is 16, brown 17 etc. 
         contours = cv2.findContours(finalMask, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
         currentColor = colours[key]
         # print(key + ': ' + str(len(contours)))
+        i = 0
         for c in contours:
-
             # The following code is only needed to show what has been detected
             # Can be removed in final version to improve fps
-            # M = cv2.moments(c)
-            # cX = int(M["m10"] / M["m00"])
-            # cY = int(M["m01"] / M["m00"])
+            M = cv2.moments(c)
+            if M["m00"] != 0:
+                cX = int(M["m10"] / M["m00"])
+                cY = int(M["m01"] / M["m00"])
+            else:
+                cX, cY = 0, 0
 
-            # cv2.circle(frame, (cX, cY), 4, (255,255,255), -1)
+            cv2.circle(frame, (cX, cY), 5, (255,255,255), -1)
 
 
-            # cv2.putText(frame, key + " ball", (cX -25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+            # if(i < 1 and str(key) != 'red'):
+            #     cv2.putText(frame, str(key), (cX -25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+            
+            # if(str(key) == 'red' and i <= 9):
+            #     cv2.putText(frame, str(key), (cX -25, cY - 25), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+
+            # i += 1
+            # Need to work out something where if a ball ends up being obscured and so won't be counted
             if(checkBallCount):
-                if(key == 'red'):
+                # When counting the number of balls make sure to reset the ball count at the beginning of the while loop
+                if (key == 'pink'):
+                    pinkCount += 1
+                elif(key == 'red'):
                     redCount += 1
-                    print('Counting red')
-                # BUG: The yellows count is always increasing for some reason
-                elif (key == 'yellow'):
-                    print('Counting yellow')
-                    yellowCount += 1
 
         # print('redCount' + str(redCount))
         # print('totalReds' + str(totalReds))
@@ -198,7 +252,7 @@ while True:
     # res = cv2.bitwise_and(frame,frame, mask = finalMask)
 
     # Display test for number of red balls
-    cv2.putText(frame, str(redCount), (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255,255,255), 2)
+    cv2.putText(frame, str(redCount), (250, 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0,0,255), 2)
 
 
     cv2.imshow('res', frame)
